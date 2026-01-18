1440a591355a74d07511189d21d75a415afacaa4
Jens Aitola <jjepaah@gmail.com>
2026-01-18 20:58:59 +0200

    Fix: Reference links break the game #2

--- js/ui.js
+++ js/ui.js
@@ -18,8 +18,16 @@ function renderPage(page) {
 
     // Remove unwanted UI elements
     article.querySelectorAll(
-        ".infobox, .toc, .metadata, .navbox, .vertical-navbox, .mw-editsection"
+        ".infobox, .toc, .mw-heading, .mw-heading2, .mw-heading3, .metadata, .navbox, .vertical-navbox, .mw-editsection, .references, .reflist, .ol.references, .reference"
     ).forEach(el => el.remove());
+
+    article.querySelectorAll("ul").forEach(ul => {
+        if (ul.querySelector("a.external")) {
+            ul.remove();
+        }
+    });
+
+    removeEmptySections(article);
 
     contentEl.innerHTML = "";
     contentEl.appendChild(article);
@@ -52,6 +60,30 @@ function renderPage(page) {
 
             loadPage(title, true);
         });
+    });
+}
+
+function removeEmptySections(article) {
+    const headers = article.querySelectorAll("h2, h3");
+
+    headers.forEach(header => {
+        let node = header.nextElementSibling;
+        let hasContent = false;
+
+        while (node && !/^H[2-3]$/.test(node.tagName)) {
+            if (
+                node.textContent.trim() !== "" ||
+                node.querySelector("img, table, ul, ol")
+            ) {
+                hasContent = true;
+                break;
+            }
+            node = node.nextElementSibling;
+        }
+
+        if (!hasContent) {
+            header.remove();
+        }
     });
 }
 
