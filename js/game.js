b36bd6326aa575566c4bdb90bd74e18d1a302ba3
Jens Aitola <jjepaah@gmail.com>
2026-01-18 12:34:49 +0200

    Added win condition functionality

--- js/game.js
+++ js/game.js
@@ -2,6 +2,10 @@ const clickCounterEl = document.getElementById("click-counter");
 const startPageEl = document.getElementById("start-page");
 const targetPageEl = document.getElementById("target-page");
 
+const winModal = document.getElementById("win-modal");
+const finalClicksEl = document.getElementById("final-clicks");
+const newRoundBtn = document.getElementById("new-round-btn");
+
 const gameState = {
     startPage: "Finland",
     targetPage: "Nikkilä",
@@ -14,18 +18,48 @@ startPageEl.textContent = gameState.startPage;
 targetPageEl.textContent = gameState.targetPage;
 clickCounterEl.textContent = gameState.clicks;
 
+
+
 async function loadPage(title, isUserClick = true) {
     if (isUserClick) {
         gameState.clicks++;
         clickCounterEl.textContent = gameState.clicks;
+
+        gameState.history.push(title);
     }
 
     const page = await fetchPage(title);
     gameState.currentPage = page.title;
-    gameState.history.push(page.title);
 
     renderPageWithTransition(page);
+
+    checkWin();
 }
 
+function checkWin() {
+    if (gameState.currentPage === gameState.targetPage) {
+        finalClicksEl.textContent = gameState.clicks;
+        winModal.classList.remove("hidden");
+        disableAllLinks();
+    }
+}
+
+function disableAllLinks() {
+    document.querySelectorAll("#content a").forEach(link => {
+        link.style.pointerEvents = "none";
+        link.style.opacity = 0.4;
+    });
+}
+
+newRoundBtn.addEventListener("click", () => {
+    winModal.classList.add("hidden");
+
+    gameState.clicks = 0;
+    clickCounterEl.textContent = 0;
+    gameState.history = [];
+    loadPage(gameState.startPage, false);
+})
+
+
 // Start here
-loadPage("Finland");
+loadPage(gameState.startPage, false);
